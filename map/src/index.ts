d73d74a949e68bfd417c9a2d994e4f034fa7a40e
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2020-01-05 15:17:27 +0530

    create multiple objects

--- map/src/index.ts
+++ map/src/index.ts
@@ -4,18 +4,10 @@ import User from "./User";
 import Company from "./Company";
 import CustomMap from "./CustomMap";
 
-const user = new User();
-const company = new Company();
-
 const map = new CustomMap("map");
-map.marker(user);
-map.marker(company);
 
-// mapboxgl.accessToken =
-//   "pk.eyJ1Ijoia3NtdXRodXVzIiwiYSI6ImNrNTBvZ2hmczBneTEzZXFuYWRocm1yYmEifQ.7mAaE2Z5SBvHSEQ3oD4YXA";
-// new mapboxgl.Map({
-//   container: "map", // container id
-//   style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-//   center: [-74.5, 40], // starting position [lng, lat]
-//   zoom: 9 // starting zoom
-// });
+let i;
+for (i = 0; i < 100; i++) {
+  map.marker(new User());
+  map.marker(new Company());
+}
